cc5ac731fa7a2bd267aa5097e5e294e710075ef5
G Srikaanth <srikaanth0912@gmail.com>
2026-08-01 23:43:53 +0530

    Add: palindrome-permutation - submission-0

---
+++ Data Structures & Algorithms/palindrome-permutation/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        if len(s)%2==0: return False
+        hm={}
+        flag=0
+        for i in s:
+            if i not in hm:
+                hm[i]=1
+            else:
+                hm[i]+=1
+        print(hm)
+        for char,count in hm.items():
+            if count%2!=0:
+                flag+=1
+            else:
+                continue
+
+
+            if(flag>1):
+                return False
+        return True if flag==1 else False
