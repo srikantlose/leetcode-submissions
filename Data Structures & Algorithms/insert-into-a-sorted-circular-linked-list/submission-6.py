7d5731aee953203aef04bc5455d74526334e36d9
G Srikaanth <srikaanth0912@gmail.com>
2026-08-02 18:49:52 +0530

    Add: insert-into-a-sorted-circular-linked-list - submission-6

---
+++ Data Structures & Algorithms/insert-into-a-sorted-circular-linked-list/submission-6.py
@@ -0,0 +1,27 @@
+# Definition for a Node.
+# class Node:
+#   def __init__(self, val=None, next=None):
+#        self.val = val
+#        self.next = next
+
+class Solution:
+    def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        if not head:
+            return Node(insertVal,head)
+        elif head.next==head:
+            head.next=Node(insertVal,head)
+            return head
+           
+        curr=head
+        prev=None
+        while curr.next:
+            prev=curr
+            
+            if curr.val<insertVal and curr.next.val>insertVal:
+                break
+            
+            curr=curr.next
+        nextRef=curr.next
+        prev.next=Node(insertVal,nextRef)
+        #curr.next.next=nextRef
+        return head
